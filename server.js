f07edbe1706d0cf607dad3bea66771d64d5a7508
Victor Parga <victorparga765@gmail.com>
2024-01-23 17:35:48 -0300

    implementando Express

--- server.js
+++ server.js
@@ -5,3 +5,22 @@ const socketIO = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+
+const PORT = process.env.PORT || 3000;
+
+
+io.on('connection', (socket) => {
+    console.log('Usuário conectado', socket.id);
+
+    socket.on('message',(data) => {
+        io.emit('message', data);
+    });
+
+    socket.on('disconnect',() => {
+        console.log('Usuário desconectado:', socket.id);
+    });
+});
+
+server.listen(PORT,() => {
+    console.log(`Servidor ouvindo na porta ${PORT}`);
+});
